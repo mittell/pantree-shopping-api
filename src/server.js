8a49c9e1bfe1da4292a2ad49f11c6df07f4e83c8
Chris Mittell <mittell@hotmail.co.uk>
2022-11-20 18:10:52 +0900

    feat: added Ingredient routes

--- src/server.js
+++ src/server.js
@@ -5,11 +5,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+const ingredientRoutes = require('./routes/ingredient.routes');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Pantree - Shopping API v1.0.0');
 });
+
+app.use('/api/ingredients', ingredientRoutes);
 
 app.listen(port, () => {
 	console.log(`App is listening on port ${port}...`);
