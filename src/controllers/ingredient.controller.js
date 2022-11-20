c894204221884f9b5db53e78136b8a32909b835c
Chris Mittell <mittell@hotmail.co.uk>
2022-11-20 18:25:05 +0900

    feat: added Ingredient controller methods

--- src/controllers/ingredient.controller.js
+++ src/controllers/ingredient.controller.js
@@ -7,7 +7,7 @@ const {
 	deleteIngredientById,
 } = require('../services/ingredient.service');
 
-const getIngredients = async (req, res, next) => {
+const getAllIngredients = async (req, res, next) => {
 	await getAllIngredients()
 		.then((ingredients) => {
 			return res.json(ingredients);
@@ -17,6 +17,71 @@ const getIngredients = async (req, res, next) => {
 		});
 };
 
+const getIngredientById = async (req, res, next) => {
+	const { id } = req.params;
+
+	await getIngredientById(id)
+		.then((ingredient) => {
+			return res.json(ingredient);
+		})
+		.catch((error) => {
+			return next(error);
+		});
+};
+
+const getIngredientByName = async (req, res, next) => {
+	const { name } = req.params;
+
+	await getIngredientByName(name)
+		.then((ingredient) => {
+			return res.json(ingredient);
+		})
+		.catch((error) => {
+			return next(error);
+		});
+};
+
+const addIngredient = async (req, res, next) => {
+	const ingredientData = req.body;
+
+	await addIngredient(ingredientData)
+		.then((ingredient) => {
+			return res.json(ingredient);
+		})
+		.catch((error) => {
+			return next(error);
+		});
+};
+
+const updateIngredientById = async (req, res, next) => {
+	const ingredientData = req.body;
+
+	await updateIngredientById(ingredientData)
+		.then((ingredient) => {
+			return res.json(ingredient);
+		})
+		.catch((error) => {
+			return next(error);
+		});
+};
+
+const deleteIngredientById = async (req, res, next) => {
+	const { id } = req.params;
+
+	await deleteIngredientById(id)
+		.then(() => {
+			return res.sendStatus(200);
+		})
+		.catch((error) => {
+			return next(error);
+		});
+};
+
 module.exports = {
-	getIngredients,
+	getAllIngredients,
+	getIngredientById,
+	getIngredientByName,
+	addIngredient,
+	updateIngredientById,
+	deleteIngredientById,
 };
